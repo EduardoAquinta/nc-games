8a6c7df8bf86cc3adf604848cfb1cefd18d54660
EduardoAquinta <edwoodashton@yahoo.co.uk>
2022-06-13 15:55:04 +0100

    Removed react bloat and added initial files

---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+        <h1>Northcoders Board Game Reviews</h1>
+        <h2>For all your Board Gaming review needs! </h2>
+        </header>
+    )
+}
