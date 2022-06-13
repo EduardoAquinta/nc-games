c5093166bd6664f1c1ae26feea9369542aa03add
EduardoAquinta <edwoodashton@yahoo.co.uk>
2022-06-13 22:44:45 +0100

    Have functionality for task 4 but styling needs work

--- src/Components/ReviewListing.jsx
+++ src/Components/ReviewListing.jsx
@@ -1,16 +1,13 @@
 export default function ReviewListing({title, designer, owner, review_img_url, category, created_at, votes, comment_count}) {
     return (
         <article id="reviewList">
-            <p>
-                Title : {title}<br></br>
-                Designer : {designer}<br></br>
-                Owner: {owner}<br></br>
-                Img: <img src={review_img_url} alt="none found" id="listing"/><br></br>
-                Category: {category}<br></br>
-                Created at: {created_at}<br></br>
-                Votes: {votes}<br></br>
-                Comment Count: {comment_count}<br></br> 
-            </p>
+            <ul className="card">
+                <li><img src={review_img_url} alt="none found" id="listing"/></li>
+                <li>Title : {title}</li>
+                <li>Designer : {designer}</li>
+                <li>Category: {category}</li>
+                <li>Votes: {votes}</li>
+            </ul>
         </article>
     )
 }
